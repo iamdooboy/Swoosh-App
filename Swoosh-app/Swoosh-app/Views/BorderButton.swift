37169704cb0edd21291f4700c65ab9b015751b45
Unknown <asianinvasion.le@gmail.com>
2018-05-16 13:05:30 -0400

    Added welcome screen

---
+++ Swoosh-app/Swoosh-app/Views/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  Swoosh-app
+//
+//  Created by Duy Le on 5/16/18.
+//  Copyright © 2018 Duy Le. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
